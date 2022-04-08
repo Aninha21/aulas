2ff1249f5cc017bee02b4e06badeefcea31a73b0
Ana <99494442+Aninha21@users.noreply.github.com>
2022-04-08 13:17:17 -0300

    Create codigo.ino

---
+++ exercicios/ex2/codigo.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(9, LOW);
+  delay(1000);
+  digitalWrite(9, HIGH);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
